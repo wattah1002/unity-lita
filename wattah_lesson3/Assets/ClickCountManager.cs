69f59a2bff08ad4514ea2fe4e062a88ecd63d613
wattah1002 <wattah.uec18@gmail.com>
2020-03-21 17:48:10 +0900

    first commit

---
+++ wattah_lesson3/Assets/ClickCountManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickCountManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int clickCount;
+    public Text clickCountText;
+    public TimeCountManager timeCountManager;
+    void Start()
+    {
+        clickCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timeCountManager.TimeCount > 0){
+            if (Input.GetMouseButtonUp(0))
+            {
+                clickCount++;
+            }
+        }
+        clickCountText.text = "クリック数：" + clickCount.ToString();
+    }
+}
---
+++ wattah_lesson3/Assets/TimeCountManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeCountManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float TimeCount;
+    public Text countDownText;
+    void Start()
+    {
+        TimeCount = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeCount -= Time.deltaTime;
+        countDownText.text = "カウントダウン：" + TimeCount.ToString("f0");
+
+        if(TimeCount <= 0){
+            countDownText.text = "ゲーム終了";
+        }
+    }
+}
