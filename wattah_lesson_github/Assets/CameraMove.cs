227979f449c2287e97f5e0c0c7844348392bd808
wattah1002 <wattah.uec18@gmail.com>
2020-03-22 12:05:53 +0900

    second commit

---
+++ wattah_lesson_github/Assets/CameraMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float posx = player.transform.position.x;
+        float posy = player.transform.position.y;
+        float posz = player.transform.position.z;
+
+        transform.position = new Vector3(posx, posy + 10, posz - 10);
+    }
+}
